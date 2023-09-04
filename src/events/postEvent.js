c9a36deeef3dde05c64c5204f53b41e19cc22269
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-04 16:08:29 +0200

    event prevent removed

--- src/events/postEvent.js
+++ src/events/postEvent.js
@@ -2,8 +2,8 @@ import postHandler from "../handlers/postHandler.js";
 import dom from "../dom.js";
 
 const postEvent = () => {
-    dom.formButton.addEventListener('click', (e) => {
-      e.preventDefault();
+    dom.formButton.addEventListener('click', () => {
+  
       postHandler();
     })
 }
