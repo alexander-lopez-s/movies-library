a04977ca7692b8c69fd7e6fd4d1d9411088e6a14
VladVladyslavHornitskyi <hornitskyy_vlad@ukr.net>
2023-09-07 11:31:24 +0200

    prevented refreshing

--- src/events/postEvent.js
+++ src/events/postEvent.js
@@ -2,7 +2,8 @@ import postHandler from "../handlers/postHandler.js";
 import dom from "../dom.js";
 
 const postEvent = () => {
-    dom.formButton.addEventListener('click', () => {
+    dom.formButton.addEventListener('click', (e) => {
+      e.preventDefault();
       postHandler();
     })
 }
